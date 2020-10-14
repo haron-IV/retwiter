1ffb1e050d156adafc2e11527c0b3432382eda5e
Bartek <bartek.bober.koder@gmail.com>
2020-10-14 23:11:18 +0200

    add env settings

--- config/browser-config.js
+++ config/browser-config.js
@@ -1,7 +1,18 @@
-module.exports = {
+const local = {
     headless: false,
     defaultViewport: null,
     args: [
         '--window-size=1920,1080'
     ]
-}
+};
+
+const prod = {
+    headless: true,
+    defaultViewport: null,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+    ]
+};
+
+module.exports = {local, prod};
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
-const browserConfig = require('./config/browser-config');
+const { local, prod } = require('./config/browser-config');
 const { login } = require('./src/login/login');
 const { retweet } = require('./src/retweet/retweet');
 
+const getEnvironment = () => {
+    const env = process.env.ENV;
+    if(env = "local") return local;
+    return prod;
+};
+
 const initPage = async () => {
-    const browser = await puppeteer.launch(browserConfig);
+    const browser = await puppeteer.launch(getEnvironment());
     const page = await browser.newPage();
     await page.goto('https://twitter.com');
 
