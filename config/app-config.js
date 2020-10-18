d39353e2c3d6fe813dfa12a873f7138c75bf3f77
Bartek <bartek.bober.koder@gmail.com>
2020-10-18 16:34:44 +0200

    add prod config

--- config/app-config.js
+++ config/app-config.js
@@ -5,7 +5,7 @@ module.exports = {
     langQuery: "?lang=en",
     selectTwittNotOlderThanHours: 2, //2
     waitMinsAfterGoToHome: 1, //1
-    waitMinsAfterRetwitt: 1, //15
-    waitMinsAfterSelectingAlreadyRetwittedPost: 1, //2
-    waitSecBeforeClickRetwittButtons: 2
+    waitMinsAfterRetwitt: 10, //15
+    waitMinsAfterSelectingAlreadyRetwittedPost: 5, //2
+    waitSecBeforeClickRetwittButtons: 3
 }
