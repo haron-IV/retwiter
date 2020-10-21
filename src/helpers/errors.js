b2ca3513b8277600921f7ffb98fd96b9f53d1d00
Bartek <bartek.bober.koder@gmail.com>
2020-10-21 23:53:05 +0200

    refactorize reinit page on error

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -2,21 +2,23 @@ const watch = require('melanke-watchjs'); //https://www.npmjs.com/package/melank
 const { state, getError, getBrowser } = require('../state/app-state');
 const { logger } = require('../logger/logger');
 const { initPage } = require('../app-management/browser');
-const { login } = require('../login/login');
 const { retwitt } = require('../retwitt/retwitt');
 
 const initAppAfterError = async () => {
-    const page = await initPage(getBrowser());
-    await login(page, process.env.USERNAME, process.env.PASSWORD);
+    const page = await initPage(await getBrowser());
     await retwitt(page);
+};
+
+const restartApp = async page => {
+    logger.error(`${getError().msg} | Application will restart.`);
+    await page.close();
+    await initAppAfterError();
+    //TODO: add debounce here - fristly check if it need to be debounced
 };
 
 const watchErrors = async page => {
     watch.watch(state, "error", async () => {
-        logger.error(`${getError().msg} | Application will restart.`);
-        await page.close();
-        await initAppAfterError();
-        //TODO: add debounce here - fristly check if it need to be debounced
+        await restartApp(page);
     });
 };
 
