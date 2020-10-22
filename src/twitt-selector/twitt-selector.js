2966df2defc5df66bb32faa2369655d217b500b6
Bartek <bartek.bober.koder@gmail.com>
2020-10-22 18:19:16 +0200

    test logs

--- src/twitt-selector/twitt-selector.js
+++ src/twitt-selector/twitt-selector.js
@@ -9,17 +9,18 @@ const twittSelector = async (page) => {
         const twittsTime = await page.$$(twittTime);
         const selectedTwitt = await selectTwitt(page, twittsTime);
         const twittLink = await page.evaluate(selectedTwitt => selectedTwitt.parentNode.getAttribute("href"), selectedTwitt);
-        
+        console.log(`--- twittlink: ${twittLink} (1)`);
         if (twittLink !== undefined) {
+            console.log(`--- twittlink: ${twittLink} (2)`);
             return twittLink;
-        }else {
-            logger.rror("twittSelector() -> twittLink is undefined");
-            setError({ msg: "Error: twittSelector() -> twittLink is undefined", appPID: process.pid });
+        } else {
+            console.log(`--- twittlink: ${twittLink} (3)`);
+            logger.error("twittSelector() -> twittLink is undefined");
+            setError({ msg: "twittSelector() -> twittLink is undefined", appPID: process.pid });
         }
         
-        
     } catch {
-        setError({ msg: "Error: twittSelector()", appPID: process.pid });
+        setError({ msg: "twittSelector()", appPID: process.pid });
     }
 };
 
