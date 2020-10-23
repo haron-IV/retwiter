92d8377067281903ebf535158e1b6554c4a371de
Bartek <bartek.bober.koder@gmail.com>
2020-10-23 20:00:32 +0200

    update script

--- scripts/missing-ubuntu-packages.sh
+++ scripts/missing-ubuntu-packages.sh
@@ -1,15 +1,17 @@
-sudo apt install libnss3
-sudo apt-get install libatk1.0-0
-sudo apt-get install libatk-bridge2.0-0
-sudo apt-get install libx11-xcb1
-sudo apt-get install libxcb-dri3-0
-sudo apt-get install libxcomposite1 
-sudo apt-get install libxdamage1
-sudo apt-get install libxfixes3
-sudo apt-get install libcups2
-sudo apt-get install libdrm2
-sudo apt-get install libxrandr2
-sudo apt-get install libgbm1
-sudo apt-get install libasound2
-sudo apt-get install libpangocairo-1.0-0
-sudo apt-get install libgtk-3-0
+sudo apt --yes install libnss3
+sudo apt-get --yes install libatk1.0-0
+sudo apt-get --yes install libatk-bridge2.0-0
+sudo apt-get --yes install libx11-xcb1
+sudo apt-get --yes install libxcb-dri3-0
+sudo apt-get --yes install libxcomposite1 
+sudo apt-get --yes install libxdamage1
+sudo apt-get --yes install libxfixes3
+sudo apt-get --yes install libcups2
+sudo apt-get --yes install libdrm2
+sudo apt-get --yes install libxrandr2
+sudo apt-get --yes install libgbm1
+sudo apt-get --yes install libasound2
+sudo apt-get --yes install libpangocairo-1.0-0
+sudo apt-get --yes install libgtk-3-0
+
+npm install forever -g
