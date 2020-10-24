98f0968d64dc982fd74231d9f6dd3ff207510860
Bartek <bartek.bober.koder@gmail.com>
2020-10-24 17:22:06 +0200

    prevent sharing your own twitts

--- src/twitt-selector/twitt-selector.js
+++ src/twitt-selector/twitt-selector.js
@@ -2,18 +2,22 @@ const { twittTime } = require('./elements');
 const selectTwitt = require('./select-twitt');
 const { setError } = require('../state/app-state');
 const { logger } = require('../logger/logger');
+const { getTwittAutor } = require('../retwitt/page-actions');
 
-const twittSelector = async (page) => {
+const twittSelector = async page => {
     try {
         await page.waitForSelector(twittTime);
         const twittsTime = await page.$$(twittTime);
         const selectedTwitt = await selectTwitt(page, twittsTime);
         const twittLink = await page.evaluate(selectedTwitt => selectedTwitt.parentNode.getAttribute("href"), selectedTwitt);
+        const twittAuthor = await getTwittAutor(page);
         if (twittLink === undefined) setError({ msg: "twittSelector() -> twittLink is undefined", appPID: process.pid });
+        if (twittAuthor === process.env.DISPLAY_USERNAME) setError({ msg: "twittSelector() -> Cannto share your own twitt", appPID: process.pid });
         
         return twittLink;
     } catch {
         setError({ msg: "twittSelector()", appPID: process.pid });
+        logger.error("twittSelector()");
     }
 };
 
