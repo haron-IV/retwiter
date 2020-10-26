6c4f20fcc12f3691c46022d240a2656b741fe15d
Bartek <bartek.bober.koder@gmail.com>
2020-10-26 15:21:13 +0100

    remove comment

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const { watchErrors } = require("./src/helpers/errors");
 const { setBrowser } = require("./src/state/app-state");
 
 // TODO: update readme and docs with maved sh files infto direcotry /scripts
-// TODO: add to script that starts applicaition killall chrome at start for clear ram (check if it is needed after running app via forever)
 // TODO: add liking all or most of recent coments from post
 
 const initApp = async () => {
